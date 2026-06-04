bf9415f7b80d90672b5d2f337e32fe10053ba459
LNP0806 <lamnhatbo0123@gmail.com>
2026-06-05 05:37:15 +0700

    refactor: remove commented-out database configuration code

--- be/src/config/db.js
+++ be/src/config/db.js
@@ -8,8 +8,6 @@ const { Pool } = require("pg");
 //   database: process.env.DB_NAME,
 // });
 
-const { Pool } = require("pg");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DB_SSL === "true"
